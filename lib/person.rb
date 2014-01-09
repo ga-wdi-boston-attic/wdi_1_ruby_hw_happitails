f3fdd47c35df503c07bbac66c42cfb78ed625bc0
Janice Smith <janice.k.n.smith@gmail.com>
2014-01-09 17:28:18 -0500

    defined classes - phase 1

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
-
+	attr_accessor :name, :age, :number_of_pets, :pet_names
+	def initialize
+		@name = name
+		@age = age
+		@number_of_pets = number_of_pets
+		@pet_names = {}
+	end
 end
--- lib/shelter.rb
+++ lib/shelter.rb
@@ -1,3 +1,9 @@
 class Shelter
-
+attr_accessor :name, :address, :clients, :number_of_animals
+	def initialize
+		@name = name
+		@address = address
+		@clients = []
+		@number_of_animals = []
+	end
 end
