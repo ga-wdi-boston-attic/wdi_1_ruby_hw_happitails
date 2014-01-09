2211ca994ebed63b69f07ad7b35ebab52b271b78
Mike Stone <mikestone14@gmail.com>
2014-01-09 17:36:37 -0500

    Created Animal, Person, and Shelter classes

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,17 @@
 class Person
+	attr_accessor :name, :age, :num_pets, :num_pets
+	def initialize(name, age, num_pets)
+		@name = name
+		@age = age
+		@num_pets = num_pets
+		@pets = {}
+	end
 
+	def add_pet(pet)
+		# @pets.merge!( (pet.name).to_sym => pet )
+	end
+
+	def remove_pet(pet)
+
+	end
 end
--- lib/shelter.rb
+++ lib/shelter.rb
@@ -1,3 +1,10 @@
 class Shelter
+	attr_accessor :name, :address, :clients, :animals, :animal, :student
+	def initialize(name, address)
+		@name = name
+		@address = address
+		@animals = {}
+		@clients = {}
+	end
 
 end
