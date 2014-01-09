7b20c83c3b58ab4b58afef19dd1552d62c8edf19
Jordan Morano <jordan.morano@gmail.com>
2014-01-09 18:22:30 -0500

    fixed person class so shelter animals hash will update

--- lib/person.rb
+++ lib/person.rb
@@ -12,11 +12,17 @@ class Person
   # instance methods for a person
   # accept a pet and add it to their pet list
   def add_pet(animal)
+    # add animal to person pet's list
     @pets.merge!({animal.name.to_sym => animal})
+    #removing animal from shelter
+    $shelter.animals.reject! { |key, value| key == animal.name}
   end
 
   # remove a pet from their list
   def remove_pet(animal)
+    # add animal to shelter
+    $shelter.animals.merge!({animal.name.to_sym => animal})
+    # removing animal from pets hash
     @pets.reject! { |key, value| key == animal.name.to_sym}
   end
 end
