56939b2bdf04520e959ec1ba96b4574c398711f7
Jordan Morano <jordan.morano@gmail.com>
2014-01-09 17:16:28 -0500

    worked on animals class - added accessors and initialize

--- lib/animal.rb
+++ lib/animal.rb
@@ -1,3 +1,12 @@
 class Animal
+  attr_accessor :name, :age, :gender, :species, :toys
 
+  def initialize(name, age, gender, species)
+    # an empty array list for toys
+    @name = name
+    @age = age
+    @gender = gender
+    @species = species
+    @toys = []
+  end
 end
