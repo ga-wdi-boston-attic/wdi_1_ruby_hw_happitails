f5109805c84274d4c63e755852862e01eede20d6
Jordan Morano <jordan.morano@gmail.com>
2014-01-09 17:39:01 -0500

    shelter class is defined and some testing done in pry

--- lib/shelter.rb
+++ lib/shelter.rb
@@ -1,3 +1,11 @@
 class Shelter
+  attr_accessor :name, :address, :animals, :clients
+
+  def initialize(name, address)
+    @name = name
+    @address = address
+    @animals = {}
+    @clients = {}
+  end
 
 end
