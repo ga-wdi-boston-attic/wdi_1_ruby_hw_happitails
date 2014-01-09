f98e0ada1834af1cfe9bfb6d8ef697023053b41f
nevermore2014 <zhou.hongyu8976@gmail.com>
2014-01-09 17:53:31 -0500

    phase1

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,17 @@
-class Person
+class People
+	attr_accessor :name, :age, :num_of_pets, :pets
+	def initialize(name, age, num_of_pets,pets)
+		@name = name
+		@age = age
+		@num_of_pets = num_of_pets
+		@pets{} = pets
+	end
 
+	def accept_pet
+
+	end
+
+	def giveup_pet
+
+	end
 end
--- lib/shelter.rb
+++ lib/shelter.rb
@@ -1,3 +1,23 @@
 class Shelter
+	attr_accessor :name, :age, :number, :clients, :pets
+	def initialize(name, age, number, clients, pets)
+		@name = name
+		@age = age
+		@clients[] = clients
+		@pets[] = pets
+	end
+
+
+	def accept_animal
+	end
+
+	def accept_client
+	end
+
+	def return_all_pets
+	end
+
+	def return_all_clients
+	end
 
 end
