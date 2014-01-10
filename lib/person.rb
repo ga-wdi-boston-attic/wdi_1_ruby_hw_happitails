e21c01f53a7a61753eb52052187391caefd96147
K Mak <karen.mak@stx.oxon.org>
2014-01-09 20:34:52 -0500

    classes defined, passed pry test, can see animal and client entries

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
+  attr_accessor :name, :age, :number_of_pets
 
+  def initialize (name, age, number_of_pets)
+    @name = name
+    @age = age
+    @number_of_pets = {}   # {Animal.name: Animal.species}
+  end
 end
--- lib/shelter.rb
+++ lib/shelter.rb
@@ -1,3 +1,10 @@
 class Shelter
+  attr_accessor :name, :address, :clients, :animals
 
+  def initialize (name, address)
+    @name = name
+    @address = address
+    @clients = {}  #  {Shelter.clients: Person.new}
+    @animals = {}  # {Shelter.animal: Animal.new}
+  end
 end
