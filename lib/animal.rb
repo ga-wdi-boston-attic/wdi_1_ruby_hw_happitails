ee22d48cceefcd0d21f636baeb34c06e6f75af74
My Name <rlbell@alum.bu.edu>
2014-01-10 00:50:42 -0500

    Added pseudocode attempt to homework

--- lib/animal.rb
+++ lib/animal.rb
@@ -1,3 +1,25 @@
+require 'pry'
 class Animal
 
+	attr_accessor  :name, :gender, :species, :toys
+
+	def initialize(name, gender, species, toys)
+		@name = name
+		@gender = gender
+		@species = species
+		@toys = toys
+  end
 end
+
+
+def all_animal_names
+ animal_names = @name
+ puts "#{animal_names}"
+end
+
+all_animals_names
+
+first_animal = Animal.new("Fluffy", "male","cat","rubbermouse")
+second_animal = Animal.new("Anders", "female", "aardvark", "string")
+
+binding.pry
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,26 @@
+require 'pry'
+
 class Person
 
+	attr_accessor  :name, :age, :number_of_pets, :pet_names
+
+	def initialize(name, age, number_of_pets, pet_names)
+		@name = name
+		@age = age
+		@number_of_pets = number_of_pets
+		@pet_names = {}
+  end
 end
+
+
+# def
+# adds a pet name to the person's pet hash
+# end
+
+# def remove_pet
+#removes a pet name from the person's hash
+# end
+
+a_person = Person.new("Sarah", "49","2",{})
+
+binding.pry
