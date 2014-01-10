0cc653a67ff999d7d87702c12fafadcab089ad80
Jordan Morano <jordan.morano@gmail.com>
2014-01-09 19:35:24 -0500

    made add_pet and remove_pet work with any shelter passed in as an arg; also made key reference consistent as string and not symbol

--- lib/person.rb
+++ lib/person.rb
@@ -11,18 +11,22 @@ class Person
 
   # instance methods for a person
   # accept a pet and add it to their pet list
-  def add_pet(animal)
+  def add_pet(animal_name, shelter)
+    # find animal object from name key
+    animal = shelter.animals[animal_name]
     # add animal to person pet's list
-    @pets.merge!({animal.name.to_sym => animal})
+    @pets.merge!({animal.name => animal})
     #removing animal from shelter
-    $shelter.animals.reject! { |key, value| key == animal.name}
+    shelter.animals.reject! { |key, value| key == animal.name}
   end
 
   # remove a pet from their list
-  def remove_pet(animal)
+  def remove_pet(animal_name, shelter)
+    # find animal object from name key
+    animal = @pets[animal_name]
     # add animal to shelter
-    $shelter.animals.merge!({animal.name.to_sym => animal})
+    shelter.animals.merge!({animal.name => animal})
     # removing animal from pets hash
-    @pets.reject! { |key, value| key == animal.name.to_sym}
+    @pets.reject! { |key, value| key == animal.name}
   end
 end
