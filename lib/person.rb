a2d5bcba6be95f88bda0b1ef880a7dc4daa533d7
Laure Fischer <laurefischer27@gmail.com>
2014-01-09 21:31:12 -0500

    defined person, animal and shelter classes

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
+  attr_accessor :name, :age, :number_of_pets, :pets
 
+  def initialize (name, age, number_of_pets)
+    @name = name
+    @age = age
+    @number_of_pets = number_of_pets
+    @pets = {}
+  end
+
+  def accept_pet # The pet is accepted by the client and removed from the shelter
+
+  end
+
+  def give_up_pet # The pet is given up and returned to the shelter
+
+  end
 end
